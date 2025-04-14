c682f2e2100071c03099a79f948350568a4e3f9d
11Khan <khansameer9226@gmail.com>
2025-04-14 23:44:15 +0500

    banners

--- components/banners/banner.tsx
+++ components/banners/banner.tsx
@@ -8,15 +8,17 @@ interface Props {
 
 function Banner({ Img, mobImg, className = "" }: Props) {
   return (
-    <div className={`cus-container relative ${className}`}>
+    <div
+      className={`cus-container relative overflow-hidden rounded-[8px] ${className}`}
+    >
       {/* Desktop/Laptop Banner */}
-      <div className="relative hidden aspect-[1148/436] max-h-[436px] w-full sm:block">
+      <div className="relative hidden aspect-[1148/436] max-h-[436px] w-full overflow-hidden rounded-[8px] sm:block">
         <Image src={Img} alt="Banner" className="h-full w-full object-cover" />
       </div>
 
       {/* Mobile Banner */}
       {mobImg && (
-        <div className="relative block aspect-[640/236] min-h-[236px] w-full sm:hidden">
+        <div className="relative block aspect-[640/236] min-h-[236px] w-full overflow-hidden rounded-[8px] sm:hidden">
           <Image
             src={mobImg}
             alt="Mobile Banner"
--- components/banners/under-price.tsx
+++ components/banners/under-price.tsx
@@ -1,3 +1,4 @@
+import { toys_banner_mob } from "@/public/assets/banner";
 import Image from "next/image";
 
 interface Props {
@@ -10,14 +11,27 @@ interface Props {
 function UnderPricerBanner({ data }: Props) {
   return (
     <div className="cus-container flex flex-col justify-start gap-[10px] md:gap-[30px]">
-      <Image
+      {/* <Image
         src={data.banner}
         alt="toy"
         className="h-[236px] w-full object-cover md:h-auto"
         width={1200}
         height={400}
-      />
-
+      /> */}
+      <div className="relative hidden aspect-[1148/199] max-h-[436px] w-full rounded-[8px] sm:block">
+        <Image
+          src={data.banner}
+          alt="Banner"
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <div className="relative block aspect-[640/236] w-full rounded-[8px] sm:hidden">
+        <Image
+          src={toys_banner_mob}
+          alt="Mobile Banner"
+          className="h-full w-full object-cover"
+        />
+      </div>
       <div className="flex w-full flex-col justify-between gap-[10px] md:flex-row md:gap-6">
         {data.underPrice.map((price, index) => (
           <div key={index} className="w-full">
